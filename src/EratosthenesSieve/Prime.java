0a583723cc9c94d4543739b56a58e9707a1648b1
Valeriia Lapytska <lerkasan@gmail.com>
2016-03-09 01:09:27 +0200

    finished Eratosthenes's Sieve hometask

---
+++ src/EratosthenesSieve/Prime.java
@@ -0,0 +1,15 @@
+package EratosthenesSieve;
+
+import java.util.stream.IntStream;
+
+public class Prime {
+
+	private boolean isPrime(long n) {
+	    return n > 1 && IntStream.rangeClosed(2, (int)Math.sqrt(n)).noneMatch(divisor -> n % divisor == 0);
+	}
+
+	private long countPrimes(int max) {
+	    return IntStream.range(1, max).parallel().filter(this::isPrime).count();
+	}
+
+}
