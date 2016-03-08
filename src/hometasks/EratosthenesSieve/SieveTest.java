9638c6ac8af53ef400c73157ae088c6a04ee833b
Valeriia Lapytska <lerkasan@gmail.com>
2016-03-09 01:19:01 +0200

    added constructorExceptionTest()

--- src/hometasks/EratosthenesSieve/SieveTest.java
+++ src/hometasks/EratosthenesSieve/SieveTest.java
@@ -1,8 +1,8 @@
 package hometasks.EratosthenesSieve;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import hometasks.Exceptions.WrongArgumentException;
 
 public class SieveTest {
 
@@ -12,6 +12,16 @@ public class SieveTest {
 		assertEquals("[2, 3]", new Sieve(3).getPrimes().toString());
 		assertEquals("[2, 3, 5, 7]", new Sieve(10).getPrimes().toString());
 		assertEquals("[2, 3, 5, 7, 11, 13, 17, 19, 23, 29]", new Sieve(30).getPrimes().toString());
+	}
+	
+	@Test(expected = WrongArgumentException.class)
+	public void constructorExceptionTest1() {
+		new Sieve(-15);
+	}
+	
+	@Test(expected = WrongArgumentException.class)
+	public void constructorExceptionTest2() {
+		new Sieve(1);
 	}
 
 }
