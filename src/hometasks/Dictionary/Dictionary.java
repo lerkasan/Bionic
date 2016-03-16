3b56fe5df81123a05a42ad9eed2712daec095cce
Valeriia Lapytska <lerkasan@gmail.com>
2016-03-16 14:25:17 +0200

    added reading dictionary from file

--- src/hometasks/Dictionary/Dictionary.java
+++ src/hometasks/Dictionary/Dictionary.java
@@ -1,8 +1,10 @@
 package hometasks.Dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-
-import hometasks.Exceptions.NullArgumentException;
+import hometasks.Exceptions.*;
 
 public class Dictionary {
 	private HashMap<String, String> words;
@@ -32,7 +34,7 @@ public class Dictionary {
 	public String getTranslation(String word) {
 		String translation = this.words.getOrDefault(word, "Нет перевода");
 		if (translation == null) {
-			return "Нет перевода";
+			return word;
 		}
 		return translation;
 	}
@@ -45,9 +47,31 @@ public class Dictionary {
 		this.putTranslation("Ok", null);	
 	}
 	
+	public void readFile(String filePath) {
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(filePath))) { 
+        	String line; 
+            while ((line = inputStream.readLine()) != null) { 
+            	String[] translation = line.split(" ");
+            	switch (translation.length) {
+            	case 2:	
+            		putTranslation(translation[0], translation[1]);
+            		break;
+            	case 1:	
+            		putTranslation(translation[0], null);
+            		break;
+            	default:
+            		throw new WrongArgumentException("Bad file format");
+            	}
+            }	   
+		} 
+        catch (IOException e) {
+        	e.printStackTrace();
+        }
+	}
+	
 	public static void main(String[] args) {
 		Dictionary dict = new Dictionary();
-		dict.init();
+		dict.readFile("D:\\dict.txt");
 		System.out.println(dict.getTranslation("Save"));
 		System.out.println(dict.getTranslation("Ok"));
 		System.out.println(dict.getTranslation("Rotate"));
