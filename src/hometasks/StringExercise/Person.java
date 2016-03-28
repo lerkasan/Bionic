3326ec39d6aaca6b4b8125bd850ad7d4098d1a00
Valeriia Lapytska <lerkasan@gmail.com>
2016-03-28 09:59:29 +0300

    added CMS hometask

---
+++ src/hometasks/StringExercise/Person.java
@@ -0,0 +1,13 @@
+package hometasks.StringExercise;
+
+public class Person {
+    String name;
+
+    public Person(String personName) {
+        name = personName;
+    }
+
+    public String greet(String yourName) {
+        return String.format("Hi %s, my name is %s", name, yourName);
+    }
+}
